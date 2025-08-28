95a6295e40afaad7767cca691aea70092f3e0a4f
Nerijusanz <nerijusanz@gmail.com>
2025-08-28 15:22:41 +0300

    62.3 Rector Params Setings #62

--- rector.php
+++ rector.php
@@ -1,24 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector;
+use Rector\Php81\Rector\Array_\FirstClassCallableRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 use RectorLaravel\Set\LaravelLevelSetList;
 
-return RectorConfig::configure()
-    ->withPaths([
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
         __DIR__.'/app',
         __DIR__.'/database',
         __DIR__.'/routes',
         __DIR__.'/src',
         __DIR__.'/tests',
-    ])
-    ->withSets([
-        LaravelLevelSetList::UP_TO_LARAVEL_120,
-    ])
-    ->withRules([
+    ]);
 
-    ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-    );
+    $rectorConfig->skip([
+        __DIR__.'/vendor',
+        ExplicitBoolCompareRector::class,
+        FirstClassCallableRector::class,
+        RemoveUselessReturnTagRector::class,
+        RemoveUselessVarTagRector::class,
+        RemoveUselessParamTagRector::class,
+    ]);
+
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses();
+    $rectorConfig->removeUnusedImports();
+
+    $rectorConfig->rule(Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector::class);
+
+    $rectorConfig->sets([
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LevelSetList::UP_TO_PHP_84,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::TYPE_DECLARATION,
+    ]);
+};
