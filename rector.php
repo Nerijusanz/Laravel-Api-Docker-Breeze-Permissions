d6994a80039e9ebba192cddfe4724923eea3401a
Nerijusanz <nerijusanz@gmail.com>
2025-09-01 15:18:30 +0300

    #82.1 Add Rector Scan Paths #82

--- rector.php
+++ rector.php
@@ -15,6 +15,7 @@ use RectorLaravel\Set\LaravelLevelSetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__.'/app',
+        __DIR__.'/bootstrap/app.php',
         __DIR__.'/database',
         __DIR__.'/routes',
         __DIR__.'/src',
