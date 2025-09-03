96f484b1deb1e336955a12815b36b88eaea10890
Nerijusanz <nerijusanz@gmail.com>
2025-09-03 17:39:28 +0300

    #96.1 Improvement: AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -15,13 +18,7 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
-
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-
-            $this->app->register(TelescopeServiceProvider::class);
-
-        }
+        $this->registerTelescope();
     }
 
     /**
@@ -29,7 +26,42 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrls();
+        $this->configureDates();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    private function configureModels(): void
+    {
         Model::shouldBeStrict(app()->isLocal());
-        DB::prohibitDestructiveCommands(app()->isProduction());
+    }
+
+    private function configureUrls(): void
+    {
+        URL::forceScheme('https');
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    private function registerTelescope(): void
+    {
+        if (app()->isLocal() && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+
+            $this->app->register(TelescopeServiceProvider::class);
+
+        }
     }
 }
